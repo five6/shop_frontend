b189b3a1cd12e40c4966c7f4014c2d9f08f8724a
five6 <936333511@qq.com>
2018-06-05 18:20:53 +0800

    api

--- src/api/goods.js
+++ src/api/goods.js
@@ -3,10 +3,16 @@ import axios from 'axios';
 const url = 'api/v1/frontend/goods';
 
 export default {
-  goods() {
-    axios.get(url);
+  fetchProducts(cond) {
+    let u = '';
+    if (cond.name) {
+      u += '?name=' + cond.name;
+    } else if (cond.page && cond.size) {
+      u += '?page=' + cond.page + '&size=' + cond.size;
+    }
+    axios.get(u);
   },
-  goodsDetail(id) {
+  goods(id) {
     axios.get(url + '/' + id);
   }
 };
--- src/store/actions.js
+++ src/store/actions.js
@@ -48,8 +48,24 @@ export default {
       commit(types.API_ERROR_500, data.message);
     }
   },
+  async fetchProducts({ commit, size, page }) {
+    const data = await goodsService.fetchProducts({ size: size, page: page });
+    if (data.code === 0) {
+      commit(types.FETCH_GOODS, data.data);
+    } else {
+      commit(types.API_ERROR_500, data.message);
+    }
+  },
   async fetchGoods({ commit, size, page }) {
     const data = await goodsService.goods(size, page);
+    if (data.code === 0) {
+      commit(types.FETCH_GOODS, data.data);
+    } else {
+      commit(types.API_ERROR_500, data.message);
+    }
+  },
+  async fetchHottest({ commit }) {
+    const data = await goodsService.fetchProducts({ name: 'hottest' });
     if (data.code === 0) {
       commit(types.FETCH_GOODS, data.data);
     } else {
